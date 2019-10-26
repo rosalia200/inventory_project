1c57e0cd24f798e45d97a3f7f0f5aff549eff587
rosalia200 <wasongarosa@gmail.com>
2019-10-26 18:39:15 +0300

    change2

--- configs/config.py
+++ configs/config.py
@@ -4,9 +4,14 @@ class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS=False
     SECRET_KEY = 'some secret'
 
-
+class Development(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:sevani@127.0.0.1:5432/inventory'### dbtype://user:password@host:port/dab_name
+    environment='development'
+    DEBUG = True
 
 class Production(Config):
     SQLALCHEMY_DATABASE_URI = 'postgres://sdoordsqoodqdj:c3b750b4cbc883970b17dbf365ff7c39b555a63e4f1dc6ecd2ea4827d630d1f3@ec2-54-221-215-228.compute-1.amazonaws.com:5432/dad41oigdlr0lb'
     DEBUG = False
     environment ='production'
+
+    
